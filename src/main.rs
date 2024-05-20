b1965ed1a94b9b5e81045a3216391c780d0352dc
ylyma <amylingyuhui@gmail.com>
2024-05-21 00:43:07 +0800

    add save file fn

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 use serde::{Deserialize, Serialize};
-use std::env::args;
+use std::{
+    env::args,
+    fs, io,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+static COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Module {
-    id: u32,
+    id: usize,
     name: String,
-    grade: Grade,
+    grade: String,
     credits: u32,
+    semester: String,
     real: bool,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub enum Grade {
-    Aplus,
-    A,
-    Bplus,
-    B,
-    Bminus,
-    Cplus,
-    C,
-    Dplus,
-    D,
-    F,
+impl Module {
+    pub fn real_new(name: String, grade: String, credits: u32, semester: String) -> Module {
+        Module {
+            id: COUNTER.fetch_add(1, Ordering::SeqCst),
+            name,
+            grade,
+            credits,
+            semester,
+            real: true,
+        }
+    }
+
+    pub fn assume_new(name: String, grade: String, credits: u32, semester: String) -> Module {
+        Module {
+            id: COUNTER.fetch_add(1, Ordering::SeqCst),
+            name,
+            grade,
+            credits,
+            semester,
+            real: false,
+        }
+    }
 }
 
 fn main() {
@@ -31,32 +48,14 @@ fn main() {
     let params = &args[1..];
     println!("command: {}", command);
     match command.as_str() {
-        "add" => {
-            execute_add(params.to_vec());
-        }
-        "edit" => {
-            execute_edit(params.to_vec());
-        }
-        "delete" => {
-            execute_delete(params.to_vec());
-        }
-        "assume" => {
-            execute_assume(params.to_vec());
-        }
-        "sort" => {
-            execute_sort(params.to_vec());
-        }
-        "show" => {
-            execute_show();
-        }
-        _ => {
-            println!("invalid command, please input any of the following commands: \n add NAME GRADE CREDITS \n edit ID \n delete ID \n assume NAME GRADE CREDITS \n sort name/semester/grade/credits \n show");
-        }
+        "add" => execute_add(params.to_vec()),
+        "edit" => execute_edit(params.to_vec()),
+        "delete" => execute_delete(params.to_vec()),
+        "assume" => execute_assume(params.to_vec()),
+        "sort" => execute_sort(params.to_vec()),
+        "show" => execute_show(),
+        _ => println!("invalid command, please input any of the following commands: \n add NAME GRADE CREDITS \n edit ID \n delete ID \n assume NAME GRADE CREDITS \n sort name/semester/grade/credits \n show"),
     }
-}
-
-pub fn parse_grade(grade: String) -> Grade {
-    Grade::A
 }
 
 pub fn execute_add(params: Vec<String>) {}
@@ -71,4 +70,12 @@ pub fn execute_sort(params: Vec<String>) {}
 
 pub fn execute_show() {}
 
-pub fn save_file() {}
+pub fn save_file() -> Result<(), io::Error> {
+    let output_path = "../data/data.json";
+    let data = {
+        let data = String::new();
+        serde_json::from_str::<Module>(&data).unwrap();
+    };
+    fs::write(output_path, serde_json::to_string_pretty(&data).unwrap())?;
+    Ok(())
+}
